637dc2abec7afdce110c75947f433b1d521c2dc4
Serhii Kovalenko <sergiikovalenko@ukr.net>
2020-04-24 09:57:22 +0300

    fix: change package.json
    change package.json

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);  
     app.useGlobalFilters(new AllExceptionsFilter());
     app.useGlobalPipes(new ValidationPipe());
-    await app.listen(process.env.PORT);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
